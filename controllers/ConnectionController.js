d617dd2bbe778517195c09e3f0e3022c15d6f9ec
Saad Muhammad <engsaad36785@gmail.com>
2023-07-04 16:16:40 +0600

    concluded initial apis

--- controllers/ConnectionController.js
+++ controllers/ConnectionController.js
@@ -1,7 +1,10 @@
 "use strict";
 const { sequelize } = require('../database/db_connect');
 const { QueryTypes } = sequelize;
+const Sequelize = require('sequelize');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const mysql = require('mysql');
+const { Pool } = require('pg');
 
 /**
  * @name createConnection
@@ -12,9 +15,9 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
  *
  * @returns {array} user
  */
-exports.createConnection = catchAsyncErrors(async ({body:{connection_name, connection_type, host, port, user_name, password, user_id }},res) => {
+exports.createConnection = catchAsyncErrors(async ({body:{connection_name, connection_type, host, port, user_name, password, user_id, default_db }},res) => {
  
-  await sequelize.query(`INSERT INTO db_connections (connection_name,connection_type, host_name,host_port, host_username, host_password, user_id ) VALUES (:connection_name, :connection_type :host_name,:host_port, :host_username, :host_password, :user_id)`,{
+  await sequelize.query(`INSERT INTO db_connections (connection_name,connection_type, host_name,host_port, host_username, host_password, user_id ) VALUES (:connection_name, :connection_type :host_name,:host_port, :host_username, :host_password, :user_id, :default_db)`,{
     replacements:{
       connection_name: connection_name,
       connection_type: connection_type,
@@ -22,7 +25,8 @@ exports.createConnection = catchAsyncErrors(async ({body:{connection_name, conne
       host_port: port,
       host_username: user_name,
       host_password: password,
-      user_id : 1 //user_id - change later
+      user_id : 1, //user_id - change later
+      default_db: default_db
     }
   });
 
@@ -31,6 +35,7 @@ exports.createConnection = catchAsyncErrors(async ({body:{connection_name, conne
     message: 'Connection created'
   });
 });
+
 /**
  * @name connectionList
  * @description get connection list.
@@ -50,6 +55,206 @@ exports.connectionList = catchAsyncErrors(async (req,res) => {
   });
 });
 
+/**
+ * @name connectionList
+ * @description test connection and return tables.
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ *
+ * @returns {array} databases
+ */
+exports.testConnection = catchAsyncErrors(async ({body:{connection_type, host, port, user_name, password }},res) => {
+  const hostConfig = {
+    user: user_name,
+    host: host,
+    password: password,
+    port: port
+  }
+  try {
+    if (connection_type ===  'mysql') {
+      const connection = mysql.createConnection(hostConfig);
+      // Connect to the MySQL host
+      connection.connect((err) => {
+        if (err) {
+          console.error('Error connecting to MySQL:', err);
+          return res.status(200).json({
+            success: false,
+            message: 'Error connecting to MySQL: '+ err
+          });;
+        }
+    
+        console.log('Connected to MySQL host successfully!');
+      
+        // Fetch the list of databases
+        connection.query('SHOW DATABASES;', (err, results) => {
+          if (err) {
+            console.error('Error fetching databases:', err);
+            return res.status(200).json({
+              success: false,
+              message: 'Error fetching databases: '+err
+            });
+          }
+      
+          const databases = results.map(row => row.Database);
+          console.log('Available MySQL databases:', databases);
+          res.status(200).json({
+            success: true,
+            message: 'Connected to MySQL host successfully!',
+            data: databases
+          });
+        });
+      
+        connection.end();
+      });
+    } else {
+      const pool = new Pool(hostConfig);
+
+      // Connect to the PostgreSQL host
+      pool.connect()
+        .then(client => {
+          console.log('Connected to PostgreSQL host successfully!');
+
+          // Fetch the list of databases
+          client.query('SELECT datname FROM pg_database WHERE datistemplate = false;')
+            .then(result => {
+              const databases = result.rows.map(row => row.datname);
+              console.log('Available PostgreSQL databases:', databases);
+              res.status(200).json({
+                success: true,
+                message: 'Connected to PostgreSQL host successfully!',
+                data: databases
+              });
+            })
+            .catch(err => {
+              console.error('Error fetching databases:', err);
+              res.status(200).json({
+                success: false,
+                message: 'Error fetching databases: '+err
+              });
+            })
+            .finally(() => {
+              client.release(); // Release the client back to the pool
+            });
+        })
+        .catch(err => {
+          console.error('Error connecting to PostgreSQL:', err);
+          res.status(200).json({
+            success: false,
+            message: 'Error connecting to PostgreSQL: '+err
+          });
+        });
+    }
+    
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({
+      success: false,
+      message: error
+    });
+  }
+});
+
+/**
+ * @name createConnection
+ * @description returns database tables.
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ *
+ * @returns {array} datalist
+ */
+exports.tableList = catchAsyncErrors(async ({query:{connection_id}},res) => {
+  const connections = await sequelize.query(`SELECT * FROM db_connections WHERE id = :connID`,{
+    replacements:{
+      connID: connection_id
+    },
+    type: QueryTypes.SELECT
+  });
+
+  if (connections.length > 0) {
+    const connection = connections[0];
+    const connConfig = {
+      username: connection.host_username,
+      password: connection.host_password,
+      database: connection.default_db,
+      host: connection.host_name,
+      port: connection.host_port,
+      dialect: connection.connection_type
+    }
+    const connSequelize = new Sequelize(connection.default_db, connection.host_username, connection.host_password, connConfig);
+
+    const datalist = await connSequelize.query(
+     `SELECT table_name FROM information_schema.tables WHERE table_schema = :db and table_type = 'BASE TABLE';`,
+    {
+      replacements:{
+        db: connection.default_db
+      },
+      type: QueryTypes.SELECT
+    });
+    console.log(datalist)
+    res.status(200).json({
+      success: true,
+      data: datalist
+    });
+  } else {
+    res.status(200).json({
+      success: true,
+      message: 'No connection Found!'
+    });
+  }
+});
+
+/**
+ * @name createConnection
+ * @description returns database views.
+ * @param {object} req
+ * @param {object} res
+ * @param {function} next
+ *
+ * @returns {array} datalist
+ */
+
+exports.viewList = catchAsyncErrors(async ({query:{connection_id}},res) => {
+  const connections = await sequelize.query(`SELECT * FROM db_connections WHERE id = :connID`,{
+    replacements:{
+      connID: connection_id
+    },
+    type: QueryTypes.SELECT
+  });
+
+  if (connections.length > 0) {
+    const connection = connections[0];
+    const connConfig = {
+      username: connection.host_username,
+      password: connection.host_password,
+      database: connection.default_db,
+      host: connection.host_name,
+      port: connection.host_port,
+      dialect: connection.connection_type
+    }
+    const connSequelize = new Sequelize(connection.default_db, connection.host_username, connection.host_password, connConfig);
+
+    const datalist = await connSequelize.query(
+      `SELECT table_name FROM information_schema.tables WHERE table_schema = :db and table_type = 'VIEW';`,
+    {
+      replacements:{
+        db: connection.default_db
+      },
+      type: QueryTypes.SELECT
+    });
+    console.log(datalist)
+    res.status(200).json({
+      success: true,
+      data: datalist
+    });
+  } else {
+    res.status(200).json({
+      success: true,
+      message: 'No connection Found!'
+    });
+  }
+});
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const route = express.Router();
 //import controller
-const { createConnection, connectionList } = require('../controllers/ConnectionController');
+const { createConnection, connectionList, testConnection, tableList, viewList } = require('../controllers/ConnectionController');
 
 
 /* -----all API routes for Report builder data----- */
 
 // connection routes
 route.route('/get-connection-list').get(connectionList);
+route.route('/test-connection').post(testConnection);
 route.route('/create-connection').post(createConnection);
+route.route('/get-tables').get(tableList);
+route.route('/get-views').get(viewList);
 
 module.exports = route
