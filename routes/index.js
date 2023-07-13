315c0de2318a5b3cbf770e9a3b43c04da3bf1df5
Saad Muhammad <engsaad36785@gmail.com>
2023-07-13 18:40:01 +0600

    initialised report controller

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const route = express.Router();
 //import controller
 const { createConnection, connectionList, testConnection, tableList, viewList, columnList, schemaList, removeConnection } = require('../controllers/ConnectionController');
 
+const { previewReport, saveReport} = require('../controllers/ReportBuilderController');
+
 
 /* -----all API routes for Report builder data----- */
 
@@ -15,5 +17,10 @@ route.route('/get-schemas').post(schemaList);
 route.route('/get-tables').get(tableList);
 route.route('/get-views').get(viewList);
 route.route('/get-columns').get(columnList);
+
+// report builder routes
+route.route('/preview-report').post(previewReport);
+route.route('/save-report').post(saveReport);
+
 
 module.exports = route
