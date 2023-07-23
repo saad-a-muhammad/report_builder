7783533d1552cc28064885914f9ebd242757c22b
Ali Newaz <ali.newaz@periscopelabs.io>
2023-07-23 17:15:56 +0600

    query execute

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -29,13 +29,13 @@ exports.createReport = catchAsyncErrors(async ({body:{ joins, connection }},res)
   }
   const connSequelize = new Sequelize(connection.default_db, connection.host_username, connection.host_password, connConfig);
 
-  // const s_table = joins.map(e=>e.to_table);
+  // const s_table = joins.map(e=>e.to_table); .substring(0,2) ${joins[0].to_table.substring(0,2)} ${p_table[0].substring(0,2)}
   
-  let query = `SELECT * FROM ${p_table[0]} ${p_table[0].substring(0,2)} ${joins[0].type} 
-                ${joins[0].to_table} ${joins[0].to_table.substring(0,2)} ON 
-                    ${joins[0].from_table.substring(0,2)}.${joins[0].from_column.column_name} = ${joins[0].to_table.substring(0,2)}.${joins[0].to_column.column_name} 
+  let query = `SELECT * FROM ${p_table[0]} ${joins[0].type} 
+                ${joins[0].to_table} ON 
+                    ${joins[0].from_table}.${joins[0].from_column.column_name} = ${joins[0].to_table}.${joins[0].to_column.column_name} 
                `;
-  if (joins.length>0) {
+  if (joins.length>1) {
     for (let i=1; i < joins.length; i++) {
       const el = joins[i];
       query+=` ${el.type} ${el.to_table} ${el.to_table.substring(0,2)} ON ${el.from_table.substring(0,2)}.${el.from_column.column_name} = ${el.to_table.substring(0,2)}.${el.to_column.column_name} `
@@ -48,6 +48,7 @@ exports.createReport = catchAsyncErrors(async ({body:{ joins, connection }},res)
   
   res.status(200).json({
     data: data,
+    query,
     success: true
   });
 });
