c0d321b6c001dd33458ace1ef8909ed28d5e3cff
shahariarptsl <shahariar@periscopelabs.io>
2023-07-30 16:24:34 +0600

    excel generated done

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -5,6 +5,10 @@ const Sequelize = require('sequelize');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const mysql = require('mysql');
 const { Pool } = require('pg');
+const moment = require('moment');
+const path = require("path");
+const ExcelJS = require("exceljs");
+const fs = require('fs');
 
 /**
  * @name previewReport
@@ -176,3 +180,153 @@ function buildReportQuery({joins, table=[], selecedCols, filters, group_by, sort
   
 };
 
+/**
+ * @name generateExcel
+ * @description Generate excel file and buffer
+ * @param {array} datalist
+ * @param {array} columns
+ * @param {object} body
+ * @param {object} res
+ *
+ * @returns {array} path
+ */
+exports.generateExcel = catchAsyncErrors(async ({ body },res) => {
+  const { datalist, columns, details } = body;
+  let fileName = '';
+  try {
+    let re_name = details['rep_name']
+    let re_desc = details['rep_desc']
+    
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet("WorkSheet", {
+      properties: { 
+        wrapText: true
+      },
+    });
+    worksheet.getRow(1).height = 40;
+    worksheet.getRow(2).height = 25;
+    worksheet.getRow(3).height = 25;
+
+    const cell = generateCells(columns.length);
+    
+    for (const cl of cell) {
+      worksheet.getColumn(cl).width = 30;
+      worksheet.getColumn(cl).alignment = { vertical: 'middle', horizontal: 'left', wrapText: true };
+    }
+    worksheet.getCell("A1").value = re_name;
+    worksheet.getCell("A1").font = { size: 16, bold: false };
+    worksheet.getCell("A1").alignment = { vertical: "middle", horizontal: "left" };
+    worksheet.mergeCells(`A1:${cell[columns.length-1]}1`);
+    worksheet.getCell("A2").value = re_desc;
+    worksheet.getCell("A2").font = { size: 11, bold: false };
+    worksheet.getCell("A2").alignment = { vertical: "middle", horizontal: "left" };
+    worksheet.mergeCells(`A2:${cell[columns.length-1]}2`);
+    const thinBlackBorder = { style: "thin", color: { argb: "FF000000" } };
+    let rowNum = 4;
+
+    columns.forEach((el, index) => {
+      worksheet.getCell(cell[index] + "3").value = el;
+      worksheet.getCell(cell[index] + "3").font = { size: 11, bold: false };
+      worksheet.getCell(cell[index] + "3").fill = {
+        type: "pattern",
+        pattern: "solid",
+        fgColor: { argb: "FFEEEEEE" },
+      };
+      worksheet.getCell(cell[index] + "3").border = {
+        top: thinBlackBorder,
+        right: thinBlackBorder,
+        bottom: thinBlackBorder,
+        left: thinBlackBorder,
+      };
+      worksheet.getCell(cell[index] + "3").alignment = {
+        vertical: "middle",
+        horizontal: "left",
+      };
+    });
+
+    if (datalist.length>0) {
+      for (const row of datalist) {
+        for (const [index, column] of columns.entries()) {
+          worksheet.getCell(cell[index] + rowNum).value =  row[column]
+          worksheet.getCell(cell[index] + rowNum).font = { size: 11, bold: false };
+          worksheet.getCell(cell[index] + rowNum).alignment = {
+            vertical: "middle",
+            horizontal: "left",
+            wrapText: true
+          };
+          worksheet.getCell(cell[index] + rowNum).border = {
+            top: thinBlackBorder,
+            right: thinBlackBorder,
+            bottom: thinBlackBorder,
+            left: thinBlackBorder,
+          };
+          
+        }
+        rowNum++;
+      }
+    } else {
+      worksheet.mergeCells(`A${rowNum}:${cell[columns.length-1]}${rowNum}`);
+      worksheet.getCell(`A${rowNum}`).value ='No Data Found';
+      worksheet.getRow(rowNum).height = 25;
+      worksheet.getCell(`A${rowNum}`).alignment = {
+        vertical: "middle",
+        horizontal: "center",
+        wrapText: true
+      };
+    }
+
+    fileName = `/excels/report_${re_name}_${moment().format("MMDDYYYY_hhmmssa")}.xlsx`; // path for file
+    const filePath = path.join(global.rootPath, `/public${fileName}`);
+    await workbook.xlsx.writeFile(filePath);
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.status(200).json({
+    success: true,
+    data: fileName
+  });
+});
+
+/**
+ * @name generateCells
+ * @description generate excel cell
+ * @param {string} count
+ *
+ */
+function generateCells(count) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const array = [];
+
+  for (let i = 0; i < count; i++) {
+    const index1 = Math.floor(i / 26);
+    const index2 = i % 26;
+
+    const element = index1 > 0 ? alphabet[index1 - 1] + alphabet[index2] : alphabet[index2];
+    array.push(element);
+  }
+
+  return array;
+}
+
+/**
+ * @name removeFile
+ * @description delete file
+ * @param {string} file_path
+ *
+ */
+exports.removeFile = catchAsyncErrors(async ({body},res) =>{
+  const { file_path } = body;
+  // const decryptedPath = AES.decrypt(file_path, 'path');
+  setTimeout(() => {
+    const filePath = path.join(global.rootPath, `/public${file_path}`);
+    fs.unlink(filePath, (err) => {
+      if (err)  console.log(err);
+    });
+  }, 30000);
+  res.status(200).json({
+    success: true,
+    massage: 'file deleted'
+  });
+})
